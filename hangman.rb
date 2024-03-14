4af764cb778569e86560e6760f807c66c3a359c3
Endand <endand2898@gmail.com>
2024-03-13 21:38:44 -0400

    Added load function
    now it can read the save data file

--- hangman.rb
+++ hangman.rb
@@ -8,6 +8,7 @@ class Hangman
       @player=Player.new
       @win=false
 
+      check_save
       play_game
    end
    
@@ -19,7 +20,8 @@ class Hangman
    end
 
    def play_game
-      puts "\nPlay Hangman!\n"
+      puts "\nPlay Hangman!\n\n"
+      puts "#{@guess_left} #{@wrong_guesses} #{@progress}"
       while @guess_left>0 && @win==false
          @gameboard.show_board
          guess = @player.take_guess
@@ -32,24 +34,56 @@ class Hangman
       #save the parameters into a new file
       File.open('saved_game.txt','w') do |file|
          file.puts guess_left.to_s
-         file.puts wrong_guesses.join(" ")
-         file.puts progress.join(" ")
+         file.puts wrong_guesses.join
+         file.puts progress.join
       end
       puts "Progress Saved Successfully."
    end
 
    def analyze_guess(guess)
       if @word.include?(guess)
+         puts "\nThat letter IS part of the secret word.\n"
          word_c=@word.chars
          word_c.each_with_index {|c,index| @progress[index]=guess if c==guess}
       else 
-         puts "\nThat letter is not part of the secret word.\n"
+         puts "\nThat letter is NOT part of the secret word.\n"
          @wrong_guesses << guess
          @guess_left-=1
       end
       @gameboard.update_board(@guess_left,@wrong_guesses,@progress)
       if @progress.join==@word
          @win=true
+      end
+   end
+
+   def check_save
+      if File.exist?('saved_game.txt')
+         puts "There is a save, would you like to load it?"
+         yn= gets.chomp.downcase
+         if yn=='y'
+            load_game
+         else
+            puts "\nStarting a New Game!\n\n"
+         end
+      end
+   end
+
+   def load_game
+      begin
+         File.open("saved_game.txt",'r') do |file|
+            file.each_with_index do |line,index|
+               case index
+               when 0
+                 @guess_left = line.to_i
+               when 1
+                 @wrong_guesses = line.chomp.split('')
+               when 2
+                 @progress = line.chomp.split('')
+               end
+            end
+         end
+      rescue 
+         puts "File not found."
       end
    end
    
@@ -64,10 +98,12 @@ class GameBoard
    end
 
    def show_board
-      puts "\nCurrent Progress: #{@progress.join}\n"
+      puts "------------------------------------------------------------------"
+      puts "Current Progress: #{@progress.join}\n"
       puts "\nWrong Guesses: #{@wrong_guesses.join(" ")}\n"
       puts "\nRemaining Letters: [#{(('a'..'z').to_a-@wrong_guesses-@progress).join(" ").upcase}]\n"
       puts "\n#{@guess_left} Trys left\n"
+      puts "------------------------------------------------------------------"
    end
 
    def update_board(guess_left,wrong_guesses,progress)
